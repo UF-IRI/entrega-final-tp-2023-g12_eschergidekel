ccd312da06bb5242ba550eb60039ccbba92a160f
vickygidekel <vicky.gidekel@outlook.com>
2023-10-25 15:41:44 -0300

    25/10 (upsi)

--- Proyecto/libreria/archivo.cpp
+++ Proyecto/libreria/archivo.cpp
@@ -12,14 +12,16 @@ eCodArchivos leerArchivoClientes(std::fstream	*archivo, Clientes *cliente){
     }
     return eCodArchivos::ExitoOperacion; //archivo binario
 }
-eCodArchivos escribirArchivoClientes(std::fstream	*archivo, Clientes *cliente){
+eCodArchivos escribirArchivoClientes(std::fstream	*archivo, Clientes *cliente)
+{
     if(!archivo->is_open())
         return eCodArchivos::ErrorApertura;
     archivo->write((char*)cliente, sizeof(Clientes));
 
     return eCodArchivos::ExitoOperacion;
 }
-eCodArchivos leerArchivoClientes(std::fstream	*archivo, Clases *clase){
+eCodArchivos leerArchivoClientes(std::fstream	*archivo, Clases *clase)
+{
     if(!archivo->is_open()) //si el archivo se pudo abrir
         return eCodArchivos::ErrorApertura;
 
--- Proyecto/libreria/archivo.h
+++ Proyecto/libreria/archivo.h
@@ -3,9 +3,11 @@
 
 #include <fstream>
 #include "clientes.h"
+#include "clases.h"
 
 typedef enum CodArchivos {ErrorApertura=-1, ErrorEscritura=0, ExitoOperacion=1} eCodArchivos;
 eCodArchivos leerArchivo(std::fstream *archivo, Clientes *cliente);
 eCodArchivos escribirArchivo(std::fstream *archivo, Clientes *cliente);
+eCodArchivos leerArchivoClientes(std::fstream	*archivo, Clases *clase);
 
 #endif // ARCHIVO_H
